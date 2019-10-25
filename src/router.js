3f59f67c0811efedf511c38fbbd275c65c404755
李子豪 <15836784857@163.com>
2019-10-25 19:14:02 +0800

    权限调整

--- src/router.js
+++ src/router.js
@@ -31,12 +31,6 @@ export default new Router({
             }, {// 权限列表
                 path: '/permissionList',
                 component: () => import('./views/permissionGroup/PermissionList.vue')
-            }, {// 角色权限列表
-                path: '/rolePermissionList',
-                component: () => import('./views/permissionGroup/RolePermissionList.vue')
-            }, {// 用户角色列表
-                path: '/userRoleList',
-                component: () => import('./views/permissionGroup/UserRoleList.vue')
             }]
         },
         {
--- src/views/permissionGroup/PermissionList.vue
+++ src/views/permissionGroup/PermissionList.vue
@@ -1,8 +1,10 @@
 <template>
     <div>
         <el-table :data="dataList" :stripe="true" :border="true">
-            <el-table-column prop="name" label="名称" width="120"></el-table-column>
-            <el-table-column prop="price" label="价格" width="100"></el-table-column>
+            <el-table-column prop="name" label="名称" width="100"></el-table-column>
+            <el-table-column prop="type" label="类型" width="120"></el-table-column>
+            <el-table-column prop="url" label="接口网址" width="120"></el-table-column>
+            <el-table-column prop="memo" label="备注"></el-table-column>
         </el-table>
         <el-pagination layout="prev,pager,next" @current-change="handleCurrentChange" :total="total"
                        :size="pageSize"></el-pagination>
